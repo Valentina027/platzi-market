fecef65e193892563f3a7d637ebf28cd8d9ac4c6
VALENTINA M <valentina.m2427@gmail.com>
2022-07-04 14:25:16 -0500

    Category MAPPER ADDED

--- src/main/java/com/platzi/market/persistence/crud/ProductoCrudRepository.java
+++ src/main/java/com/platzi/market/persistence/crud/ProductoCrudRepository.java
@@ -5,9 +5,11 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface ProductoCrudRepository extends CrudRepository  <Producto, Integer> {
 
-    List<Producto> findByIdCategoria(int idCategoria);
+    List<Producto> findByIdCategoriaOrderByNombreAsc(int idCategoria);
+    Optional<List<Producto>> findByCantidadStockLessThanAndEstado (int cantidadStock, boolean estado);
 
 }
